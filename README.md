340fa0db9b4d546caf7371b17deef890d770f62f
Princerwtt <109853314+Princerwtt@users.noreply.github.com>
2022-10-03 20:48:19 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-function
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-puawa7)
---
+++ script.js
@@ -0,0 +1,8 @@
+let userInput = 21;
+let result;
+
+result = 18 + userInput;
+
+result = 18 + 21 - (13 * 20) / 5;
+console.log(result);
+console.log(userInput);
